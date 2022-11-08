b19cfad141add5ce46bb3bb3b62aa9c68caabbc0
Alan <dev@bunburya.eu>
2022-11-08 22:15:22 +0000

    Fixing tests for FeedList

--- test/fetch_test_feeds.py
+++ test/fetch_test_feeds.py
@@ -16,6 +16,7 @@ logging.getLogger().setLevel(logging.INFO)
 import requests
 from rss_digest.feeds import parse_opml_file, FeedList
 
+
 def main(content_dir: str, *opml_files: str):
     now_utc = datetime.utcnow()
     today_dir = os.path.join(content_dir, now_utc.strftime('%Y-%m-%d'))
@@ -26,14 +27,14 @@ def main(content_dir: str, *opml_files: str):
         os.makedirs(log_dir)
     log_file = os.path.join(log_dir, now_utc.strftime('%Y-%m-%d.log'))
 
-    #logging.basicConfig(filename=log_file)
+    # logging.basicConfig(filename=log_file)
     logging.info(f'Fetching RSS feeds at {now_utc:%H:%M:%S UTC on %Y-%m-%d}.')
 
     for opml_file in opml_files:
         logging.info(f'Checking OPML file {opml_file}')
         feedlist = parse_opml_file(opml_file)
         for feed in feedlist:
-            rss_file = os.path.join(today_dir, feed.name.replace('/', '__slash__').replace('.', '__dot__')+'.xml')
+            rss_file = os.path.join(today_dir, feed.name.replace('/', '__slash__').replace('.', '__dot__') + '.xml')
             try:
                 r = requests.get(feed.xml_url)
                 r.raise_for_status()
@@ -48,6 +49,8 @@ def main(content_dir: str, *opml_files: str):
     end_utc = datetime.utcnow()
     logging.info(f'Finished fetching RSS feeds at {end_utc: %H:%M:%S UTC}')
 
+
 if __name__ == '__main__':
     from sys import argv
+
     main(*argv[1:])
