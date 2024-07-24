df868fba01ded3dccfbb20b97151d943a998fbaa
bunburya <dev@bunburya.eu>
2024-07-24 13:31:43 +0100

    fixing wildcard issue

--- rss_digest/feedlist.py
+++ rss_digest/feedlist.py
@@ -48,10 +48,13 @@ class FeedSearch:
     """
 
 <<<<<<< Updated upstream:rss_digest/feedlist.py
+<<<<<<< Updated upstream:rss_digest/feedlist.py
     title: Optional[str] = WILDCARD
     xml_url: Optional[str] = WILDCARD
     category: Optional[str] = WILDCARD
 =======
+=======
+>>>>>>> Stashed changes:rss_digest/feeds.py
     name: Optional[str] = None
     xml_url: Optional[str] = None
     category: Optional[str] = None
@@ -63,6 +66,9 @@ class FeedSearch:
             self.xml = WILDCARD
         if self.category is None:
             self.category = WILDCARD
+<<<<<<< Updated upstream:rss_digest/feedlist.py
+>>>>>>> Stashed changes:rss_digest/feeds.py
+=======
 >>>>>>> Stashed changes:rss_digest/feeds.py
 
     def __eq__(self, other):
